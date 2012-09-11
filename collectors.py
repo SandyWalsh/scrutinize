aa434cda220f9da6d9b99dd264c2ba7c42d9abd2
SandyWalsh <sandy.walsh@rackspace.com>
2012-09-11 12:22:52 -0300

    good refactoring ... much cleaner

--- collectors.py
+++ collectors.py
@@ -8,36 +8,30 @@ class Collector(object):
         self.notifier_dict = {}  # {name: handle}
         print self.__class__.__name__, configuration
 
-    def send_to_notifiers(self, label, value):
-        for name in self.notifiers:
-            notifier = self.notifier_dict.get(name)
-            if notifier:
-                notifier.send(label, value)
-
-    def start(self, target_config):
+    def start(self):
         # NOTE: Don't store any state in this collector.
         # It will be reused many times. Return a state object
         # here and it will be given back to you in stop()
         return None
 
-    def stop(self, state, target_config):
+    def stop(self, state):
         pass
 
 
 class Profile(Collector):
-    def start(self, target_config):
-        print "Profile.start", target_config
+    def start(self):
+        print "Profile.start"
 
-    def stop(self, state, target_config):
-        print "Profile.stop", target_config
+    def stop(self, state):
+        print "Profile.stop"
 
 
 class Time(Collector):
-    def start(self, target_config):
-        print "Time.start", target_config
+    def start(self):
+        print "Time.start"
         return time.time()
 
-    def stop(self, state, target_config):
+    def stop(self, state):
         elapsed = time.time() - state
-        print "Time.stop", target_config, elapsed
+        print "Time.stop", elapsed
         return elapsed
--- scrutinize.py
+++ scrutinize.py
@@ -41,108 +41,85 @@ def find_code(target):
     return getattr(klass_object, function)
 
 
-def plugin_wrapper(inner, plugin, config, label, label_extractor):
+def load_plugins(config):
+    plugin_dict = {}
+    if config:
+        for name, info in config.iteritems():
+            code = info['code']
+            plugin_config = info.get('config', {})
+            plugin_class = find_code(code)
+            print "Loaded plugin '%(name)s' from %(code)s" % locals()
+            plugin = plugin_class(plugin_config)
+            plugin_dict[name] = plugin
+    return plugin_dict
+
+
+def plugin_wrapper(bundle):
     def defer(*args, **kwargs):
-        state = plugin.start(config)
+        state = bundle.collector.start()
         result = None
-        final_label = label
-        if label_extractor:
-            final_label = label_extractor.extract(*args, **kwargs) or label
+        label = bundle.label
+        if bundle.label_extractor:
+            label = bundle.label_extractor.extract(*args, **kwargs) or \
+                                            bundle.label
         try:
-            result = inner(*args, **kwargs)
+            result = bundle.target_impl(*args, **kwargs)
         finally:
-            value = plugin.stop(state, config)
-            plugin.send_to_notifiers(final_label, value)
+            value = bundle.collector.stop(state)
+            bundle.notify(label, value)
         return result
 
     return defer
 
 
-def inject_code(target, new_impl, command_config, label_extractor):
-    label = None
-    if command_config:
-        label = command_config.get("label", label)
-    module, klass, function = get_module(target)
-    if not klass:
-        orig = getattr(sys.modules[module], function)
-        label = label or "%(module)s.%(function)s" % locals()
-        setattr(sys.modules[module], function, plugin_wrapper(orig, new_impl,
-                                       command_config, label, label_extractor))
-        return orig
+class Bundle(object):
+    def __init__(self, label, bundle, collectors, label_extractors, notifiers):
+        self.label = label
+        self.target = bundle["target"]
+        collector_name = bundle["collector"]
+        label_extractor_name = bundle.get("label_extractor")
+        notifier_names = bundle.get("notifiers", [])
 
-    klass_object = getattr(sys.modules[module], klass)
-    label = label or "%(module)s.%(klass)s.%(function)s" % locals()
-    orig = getattr(klass_object, function)
-    setattr(klass_object, function, plugin_wrapper(orig, new_impl,
-                                      command_config, label, label_extractor))
-    return orig
+        self.label_extractor = None
+        if label_extractor_name:
+            self.label_extractor = label_extractors.get(label_extractor_name)
 
+        self.send_list = [notifiers.get(name) for name in notifier_names
+                                                       if name]
+        self.collector = collectors.get(collector_name)
 
-def reset_code(target, old_impl, command_config):
-    module, klass, function = get_module(target)
-    if not klass:
-        orig = getattr(sys.modules[module], function)
-        setattr(sys.modules[module], function, old_impl)
-        return orig
+        self.target_impl = None  # to be replaced on patch.
 
-    klass_object = getattr(sys.modules[module], klass)
-    orig = getattr(klass_object, function)
-    setattr(klass_object, function, old_impl)
-    return orig
+    def inject(self):
+        print "Monkeypatching '%s' plugin on %s" % \
+                        (self.collector.__class__.__name__, self.target)
 
+        module, klass, function = get_module(self.target)
+        if not klass:
+            self.target_impl = getattr(sys.modules[module], function)
+            setattr(sys.modules[module], function, plugin_wrapper(self))
+            return
 
-def load_plugins(config):
-    if config:
-        for name, info in config.iteritems():
-            code = info['code']
-            plugin_config = info.get('config', {})
-            collector_class = find_code(code)
-            print "Loaded plugin '%(name)s' from %(code)s" % locals()
-            collector = collector_class(plugin_config)
-            info['impl'] = collector
-    return config
+        klass_object = getattr(sys.modules[module], klass)
+        self.target_impl = getattr(klass_object, function)
+        setattr(klass_object, function, plugin_wrapper(self))
 
+    def reset(self):
+        print "Resetting '%s' plugin from %s" % \
+                        (self.collector.__class__.__name__, self.target)
+        module, klass, function = get_module(bundle.target)
+        if not klass:
+            setattr(sys.modules[module], function, bundle.target_impl)
+        else:
+            klass_object = getattr(sys.modules[module], klass)
+            setattr(klass_object, function, bundle.target_impl)
 
-def inject_collectors(collectors, config, label_extractor_dict):
-    for name, info in collectors.iteritems():
-        plugin_impl = info['impl']
+        bundle.target_impl = None
 
-        # See if we have any hooks for this collector in the
-        # global configuration.
-        plugin_commands = config.get(name)
-        if not plugin_commands:
-            continue
-
-        # a 1-1 list of plugin_config that holds underlying impls
-        original_impls = []
-        for command in plugin_commands:
-            target = command['target']
-            command_config = command.get('config', {})
-            label_extractor_name = command.get("label_extractor")
-            label_extractor = label_extractor_dict.get(label_extractor_name)
-            print "Monkeypatching '%s' plugin on %s" % (name, target)
-            target_impl = inject_code(target, plugin_impl, command_config,
-                                      label_extractor)
-            original_impls.append(target_impl)
-
-        info['original_impls'] = original_impls
-
-
-def reset_collectors(collectors, config):
-    for name, info in collectors.iteritems():
-        # See if we have any hooks for this collector in the
-        # global configuration.
-        plugin_commands = config.get(name)
-        if not plugin_commands:
-            continue
-
-        # a 1-1 list of plugin_config that holds underlying impls
-        original_impls = info['original_impls']
-        for command in plugin_commands:
-            original_impl = original_impls.pop(0)
-            target = command['target']
-            print "Rewinding '%s' plugin on %s" % (name, target)
-            target_impl = reset_code(target, original_impl, None)
+    def notify(self, label, value):
+        for notifier in self.send_list:
+            if notifier:
+                notifier.send(label, value)
 
 
 #--------------------------
@@ -179,30 +156,23 @@ if __name__ == '__main__':
     collectors = load_plugins(config.get('collectors'))
     label_extractors = load_plugins(config.get('label_extractors'))
 
-    # Initialize the collectors with the notifiers and label extractors.
-    # It would be nice to do this in load_plugin but I can't think of
-    # a clean way to do it. So, while we're at it, make the
-    # plugin maps a little cleaner.
-    notifier_dict = {}
-    for name, info in notifiers.iteritems():
-        notifier_dict[name] = info['impl']
-    labelex_dict = {}
-    for name, info in label_extractors.iteritems():
-        labelex_dict[name] = info['impl']
-    for name, info in collectors.iteritems():
-        impl = info['impl']
-        impl.notifier_dict = notifier_dict
-        impl.label_extractor_dict = labelex_dict
+    del config['notifiers']
+    del config['collectors']
+    del config['label_extractors']
 
-    inject_collectors(collectors, config, labelex_dict)
+    bundles = [Bundle(label, bundle, collectors, label_extractors, notifiers)
+                    for label, bundle in config.iteritems()]
+
+    for bundle in bundles:
+        bundle.inject()
 
     # Try some
     function_a("label.from.extractor", 2, 3, 4)
     b = Blah()
     print b.method_b(10, 20, 30, 40)
 
-    reset_collectors(collectors, config)
+    for bundle in bundles:
+        bundle.reset()
 
     function_a("label.from.extractor", 2, 3, 4)
     print b.method_b(10, 20, 30, 40)
-
